ebcf3161b7ed7fa2610560074042383988743e34
XU Ximing <xmngxu@gmail.com>
2023-04-20 08:22:14 +0800

    Update rdkit2pdbqt.py

--- pywatvina/watvina/rdkit2pdbqt.py
+++ pywatvina/watvina/rdkit2pdbqt.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 '''
 © Copyright 2015, Maciej Wojcikowski Revision caf5d84a.
-Wójcikowski, M., Zielenkiewicz, P. & Siedlecki, P. Open Drug Discovery Toolkit (ODDT): a new open-source player in the drug discovery field. J Cheminform 7, 26 (2015). https://doi.org/10.1186/s13321-015-0078-2
+Wojcikowski, M., Zielenkiewicz, P. & Siedlecki, P. 
+Open Drug Discovery Toolkit (ODDT): a new open-source player in the drug discovery field. 
+J Cheminform 7, 26 (2015). https://doi.org/10.1186/s13321-015-0078-2
 
 Modified by XU Ximing
 xuximing@ouc.edu.cn
@@ -645,7 +647,7 @@ if __name__ == "__main__":
                     at_idx = problem.GetAtomIdx()
                     atom = mol.GetAtomWithIdx(at_idx)
                     chg = atom.GetFormalCharge()
-                    print(f'Atom N {at_idx} with formal charge {chg}')
+                    print(f'REMARK  N {at_idx} with formal charge {chg}')
                     atom.SetFormalCharge(1)
                     atom.UpdatePropertyCache()
                 else:
